6be9396caedc26ada0f488012d38b14402e99300
aksmith5239 <aklobby@gmail.com>
2021-01-20 14:53:04 -0700

    changes to appointments page

--- client/src/Pages/OrderHistory.js
+++ client/src/Pages/OrderHistory.js
@@ -16,7 +16,7 @@ function OrderHistory() {
     <>
       <div className="container my-1">
         <Link to="/">
-          ← Back to Our Services
+          ← Back to Our Style Gallery
           </Link>
 
         {user ? (
